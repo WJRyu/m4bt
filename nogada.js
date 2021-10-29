8849174395f338e22b6947e2bfa7b1629f25dcaf
WJRyu <lunez@theheartcompany.kr>
2021-10-29 11:22:48 +0900

    .

--- nogada.js
+++ nogada.js
@@ -1487,7 +1487,7 @@ function next() {
 	}
 	   else if(res=="BM_M" && res3=="CM_M" && res7=="EI_I" && res11=="SG_G"){
 		$(".nn1").html(n_list["n16"]["name"]);
-    //  $(".img").attr('src', './img/ISBR.png;');
+      $(".img").attr('src', './img/ISBR.png;');
 		   m4= new M4BT(n_list["n16"]["name"]);
     //   $(".result").attr('style', 'background-color:#f5deb3;');
       // $(".result1").attr('style', 'background-color:#f5deb3;');
